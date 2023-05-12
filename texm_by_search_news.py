60f865bb77b58b86f1c3f18999492a7bfc403a1e
Marek <133262986+MLiwandowski@users.noreply.github.com>
2023-05-12 22:52:33 +0300

    Update texm_by_search_news.py

--- texm_by_search_news.py
+++ texm_by_search_news.py
@@ -1,47 +1,47 @@
-import subprocess
 import asyncio
-from telegram import Bot
 import json
 from difflib import SequenceMatcher
-import time
+from telegram import Bot
+import requests
+from bs4 import BeautifulSoup
+import asyncio
 from random import randrange
+from typing import List
+import time
 
-# ////////////////////////////////////////////////////////////
-# Define the file path
-# script_path_1 = "./news_search_1.py"
-# script_path_2 = "./news_search_2.py"
-# script_path_n... = "./news_search_n... .py"
+URL_IXBT = "https://www.ixbt.com/news/?show=tape"
+URL_GAGA = "https://gagadget.com/news/"
+KEYWORDS = ["Apple", "Blackview", "Google", "Iphone", "LG", "Lynk & Co", "Motorola", "Nintendo Switch",
+            "Pixel", 'Realme', "Sharp", "Tecno", "Xiaomi", "Samsung", "Nokia", "Tesla", "PlayStation", "смартфон"]
 
-# Run the script
-# subprocess.call(["python", script_path_1])
-# subprocess.call(["python", script_path_2])
+headers = {
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+}
 
-# ////////////////////////////////////////////////////////////
+def get_articles_urls_ixbt(url: str) -> str:
+    with requests.Session() as s:
+        response = s.get(url=url, headers=headers)
 
-words = ["Apple", "Blackview", "Google", "Iphone", "LG", "Lynk & Co", "Motorola", "Nintendo Switch", "Pixel", 'Realme', "Sharp","Tecno","Xiaomi", "Samsung", "Nokia", "Tesla ", "PlayStation", "смартфон"]
-keywords = words
-
-#////////////////////////////////////////////////////////////
-
-url_ixbt = "https://www.ixbt.com/news/?show=tape"
-
-def get_articles_urls(url):
-    s = requests.Session()
-    response = s.get(url=url, headers=headers)
-    soup = BeautifulSoup(response.text, "lxml")
+    soup = BeautifulSoup(response.text, "html.parser")
     articles_urls = soup.find_all("h2", class_="no-margin")
+
     articles_urls_list = []
     for au in articles_urls:
         art_url = "https://www.ixbt.com/" + au.find("a")["href"]
         articles_urls_list.append(art_url)
+
     time.sleep(randrange(2, 5))
     with open("articles_url_list_1.txt", "w", encoding="utf-8") as file:
         for url in articles_urls_list:
             file.write(f"{url}\n")
+
     return "Работа по сбору ссылок выполнена"
 
+get_articles_urls_ixbt(URL_IXBT)
 
-def get_data(file_path, keywords):
+def get_data_ixbt(file_path: str, keywords: List[str]) -> None:
     with open(file_path) as file:
         url_list = [line.strip() for line in file.readlines()]
 
@@ -54,54 +54,48 @@ def get_data(file_path, keywords):
         soup = BeautifulSoup(response.text, "lxml")
 
         articles = soup.find_all("div", class_="b-article__header")
-        article_title = articles[0].find("h1").text.strip()
-        article_img = f"https://www.ixbt.com{soup.find('figure', class_='image-caption').find('img').get('src')}"
+        if articles:
+            article_title = articles[0].find("h1").text.strip()
+            article_img = f"https://www.ixbt.com{soup.find('figure', class_='image-caption').find('img').get('src')}"
 
-        article_text = []
-        for p in soup.find_all("p"):
-            article_text.append(p.text.strip().replace('\n', ''))
-        article_text = '\n'.join(article_text)
-        article_text = article_text[:article_text.rfind(' 2023 в ')]
+            article_text = []
+            for p in soup.find_all("p"):
+                article_text.append(p.text.strip().replace('\n', ''))
+            article_text = '\n'.join(article_text)
+            article_text = article_text[:article_text.rfind(' 2023 в ')]
 
-        # Check if article contains any of the keywords
-        if any(keyword.lower() in article_text.lower() for keyword in keywords):
-            article_text = article_text.split('\n')
-            # Remove everything after the publication date
-            publication_date_index = next((i for i, s in enumerate(article_text) if 'Дата публикации' in s), None)
-            if publication_date_index is not None:
-                article_text = '\n'.join(article_text[:publication_date_index])
+            # Check if article contains any of the keywords
+            if any(keyword.lower() in article_text.lower() for keyword in keywords):
+                article_text = article_text.split('\n')
+                # Remove everything after the publication date
+                publication_date_index = next((i for i, s in enumerate(article_text) if 'Дата публикации' in s), None)
+                if publication_date_index is not None:
+                    article_text = '\n'.join(article_text[:publication_date_index])
+                else:
+                    article_text = '\n'.join(article_text)
+
+                print(f"{article_title}\n{article_text}\n{article_img}\n{'#' * 10}")
+                result_data.append(
+                    {
+                        "original_url": url,
+                        "article_title": article_title,
+                        "article_img": article_img,
+                        "article_text": article_text,
+                    }
+                )
             else:
-                article_text = '\n'.join(article_text)
-
-            print(f"{article_title}\n{article_text}\n {article_img}\n{'#' * 10}")
-            result_data.append(
-                {
-                    "original_url": url,
-                    "article_title": article_title,
-                    "article_img": article_img,
-                    "article_text": article_text,
-                }
-            )
+                print(f"Слова не найдены в статье {article_title}")
         else:
-            print(f"Слова не найдены в статье {article_title}")
-        print(f"Обработал {idx + 1}/{urls_count}")
+            print(f"Статья не найдена для URL: {url}")
+
+        print(f"Обработано {idx + 1}/{urls_count}")
 
     with open("result1.json", "w", encoding="utf-8") as file:
         json.dump(result_data, file, indent=4, ensure_ascii=False)
 
-def main():
-    get_articles_urls(url=url_ixbt)
-    get_data("articles_url_list_1.txt", keywords)
+get_data_ixbt('articles_url_list_1.txt', KEYWORDS)
 
-if __name__ == "__main__":
-    main()
-# ///////////////////////////////////////////////////////////
-WORDS = ["Apple", "Blackview", "Google", "Iphone", "LG", "Lynk & Co", "Motorola", "Nintendo Switch", "Pixel", 'Realme', "Sharp","Tecno","Xiaomi", "Samsung", "Nokia", "Tesla ", "PlayStation", "смартфон"]
-KEYWORDS = [word.lower() for word in WORDS]
-
-URL_IXBT = "https://gagadget.com/news/"
-
-def get_articles_urls(url: str) -> str:
+def get_articles_urls_gagadget(url: str) -> str:
     with requests.Session() as s:
         response = s.get(url=url, headers=headers)
 
@@ -112,14 +106,17 @@ def get_articles_urls(url: str) -> str:
     for au in articles_urls:
         art_url = "https://gagadget.com/" + au.find("a")["href"]
         articles_urls_list.append(art_url)
+
     time.sleep(randrange(2, 5))
     with open("articles_url_list_2.txt", "w", encoding="utf-8") as file:
         for url in articles_urls_list:
             file.write(f"{url}\n")
+
     return "Работа по сбору ссылок выполнена"
 
+get_articles_urls_gagadget(URL_GAGA)
 
-def get_data(file_path: str, keywords: List[str]) -> None:
+def get_data_gagadget(file_path: str, keywords: List[str]) -> None:
     with open(file_path) as file:
         url_list = [line.strip() for line in file.readlines()]
 
@@ -142,8 +139,6 @@ def get_data(file_path: str, keywords: List[str]) -> None:
             article_text = '\n'.join(article_text)
             article_text = article_text[:article_text.rfind('Источник: ')]
 
-            # print(f"{article_title}\n{article_text}\n {article_img}\n{'#' * 10}")
-
             # Check if article contains any of the keywords
             if any(keyword.lower() in article_text.lower() for keyword in keywords):
                 article_text = article_text.split('\n')
@@ -154,7 +149,7 @@ def get_data(file_path: str, keywords: List[str]) -> None:
                 else:
                     article_text = '\n'.join(article_text)
 
-                print(f"{article_title}\n{article_text}\n {article_img}\n{'#' * 10}")
+                print(f"{article_title}\n{article_text}\n{article_img}\n{'#' * 10}")
                 result_data.append(
                     {
                         "original_url": url,
@@ -173,18 +168,9 @@ def get_data(file_path: str, keywords: List[str]) -> None:
     with open("result2.json", "w", encoding="utf-8") as file:
         json.dump(result_data, file, indent=4, ensure_ascii=False)
 
+get_data_gagadget("articles_url_list_2.txt", KEYWORDS)
 
-def main():
-    get_articles_urls(url=URL_IXBT)
-    get_data("articles_url_list_2.txt", KEYWORDS)
-
-
-if __name__ == "__main__":
-    main()
-
-
-# ////////////////////////////////////////////////////////////
-# Read data from result1.json and result2.json
+# ///////////////////////////////////////////////////////////
 with open('result1.json', 'r', encoding="utf-8") as file1, \
         open('result2.json', 'r', encoding="utf-8") as file2:
 
@@ -266,7 +252,7 @@ async def publish_articles():
 
             # Add the article to the published articles list
             new_articles.append(article)
-            time.sleep(randrange(2, 5))
+            await asyncio.sleep(randrange(2, 5))
 
     # Update the published articles record
     published_articles.extend(new_articles)
@@ -275,5 +261,3 @@ async def publish_articles():
 
 # Run the publish_articles coroutine
 asyncio.run(publish_articles())
-# ////////////////////////////////////////////////////////////////
-
